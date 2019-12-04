f05c9b5d77276de7fa3fc5de6a0406b57a4eda67
luanyi <luanyi@uw.edu>
2019-12-03 23:21:31 -0800

    fix run.zsh

--- preprocessing/ace2005/run.zsh
+++ preprocessing/ace2005/run.zsh
@@ -5,7 +5,7 @@ for i in English/*/timex2norm/*.sgm
 do
     echo $i
     python3 ace2ann.py `echo $i | sed -e 's/.sgm/.apf.xml/g'` $i result/`basename $i .sgm`.txt >! result/`basename $i .sgm`.ann
-done &>! log 
+done #&>! log 
 # split & parse
 mkdir text
 java -cp ".:../common/stanford-corenlp-full-2015-04-20/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -props ../common/props_ssplit -outputFormat conll -filelist train_list -outputDirectory text/
@@ -18,7 +18,7 @@ done
 for i in text/*.split.txt
 do
     echo $i && python3 ../common/standoff.py result/`basename $i .split.txt`.txt result/`basename $i .split.txt`.ann $i >! text/`basename $i .split.txt`.split.ann
-done &>! log 
+done #&>! log 
 # fix sentence split errors
 mkdir fixed
 for i in text/*.split.txt
@@ -42,14 +42,14 @@ cd ..
 for i in corpus/*.split.txt
 do
     echo $i && python3 ../common/standoff.py corpus/`basename $i .split.txt`.txt corpus/`basename $i .split.txt`.ann $i >! corpus/`basename $i .split.txt`.split.ann
-done &>! log &
+done #&>! log &
 # conll to so
 for i in fixed/*.split.txt
 do
     echo $i && perl ../common/dep2so.prl fixed/`basename $i .split.txt`.txt.conll $i >! corpus/`basename $i .txt`.stanford.so
 done
 # split into train, dev, test
-mkdir corpus/train corpus/dev corpus/test corpus/train+dev
+mkdir corpus/train corpus/dev corpus/test 
 for i in `cat split/train`
 do
 mv corpus/$i* corpus/train
@@ -62,4 +62,4 @@ for i in `cat split/test`
 do
 mv corpus/$i* corpus/test
 done
-cp corpus/train/* corpus/dev/* corpus/train+dev/
+cp corpus/train/* corpus/dev/* 
